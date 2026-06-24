d4513823cb4f0a78628d620c1491303e0c65a321
drabiot <tchartie@student.42angouleme.fr>
2026-06-24 15:45:51 +0200

    Init Describe program

--- README.md
+++ README.md
@@ -7,7 +7,7 @@
 
 Data Science × Logistic Regression.<br>
 In this project, we will continue our journey of the machine learning.<br>
-We will learn how to read a data set, visualize it in different ways, and select and clean unnecessary information.<br>
+We will learn how to read a data set, visualize it in different ways, and select and clean unnecessary information.
 & we will train logistic regression model that will solve classification problem.
 
 ![Static Badge](https://img.shields.io/badge/language-python_3-yellow)
--- describe.py
+++ describe.py
@@ -6,18 +6,37 @@
 #    By: tchartie <tchartie@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2026/06/24 14:22:11 by tchartie          #+#    #+#              #
-#    Updated: 2026/06/24 14:38:33 by tchartie         ###   ########.fr        #
+#    Updated: 2026/06/24 15:45:10 by tchartie         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 from sys import argv
 from pandas import read_csv, DataFrame
+import numpy as np
+
+def describe(data : DataFrame):
+	numeric = data.select_dtypes(include='number').drop(columns=['Index'], errors='ignore')
+
+	stats = {
+		'count': numeric.count(),
+		'mean':  numeric.mean(),
+		'std':   numeric.std(),
+		'min':   numeric.min(),
+		'25%':   numeric.quantile(0.25),
+		'50%':   numeric.quantile(0.50),
+		'75%':   numeric.quantile(0.75),
+		'max':   numeric.max(),
+	}
+		
+	result = DataFrame(stats).T
+	print(result)
+	return 0
 
 def main():
 	try:
 		assert len(argv) == 2
 		data : DataFrame = read_csv(argv[1])
-		print(data)
+		describe(data)
 	except:
 		return (1)
 	return (0)
